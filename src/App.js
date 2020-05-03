735d00bcffd06b966e73b922da796837473e3140
Matteo Spreafico <matsprea@users.noreply.github.com>
2020-05-03 09:48:37 +0100

    Would you rather - Header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>{' '}
+      <header className="App-header">Would You Rather</header>
     </div>
   );
 }
